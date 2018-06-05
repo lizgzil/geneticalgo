f44b9c34e3d9ea33cdd8185610aa3e9df432dee8
Elizabeth Gallagher <lizgzil@hotmail.com>
2018-06-05 17:41:53 +0100

    make a colour square

--- ga.py
+++ ga.py
@@ -1,3 +1,27 @@
 import pandas as pd
+import numpy as np
 
-dfkhsdkfj
+from pylab import imshow, show, get_cmap
+from numpy import random
+
+
+
+#Z = random.random((5,5,3))   # Test data
+
+# 4x3: rgb 
+Z = [[[0.1,0.9,0.9],[0.9,0.1,0.9]],
+[[0.1,0.9,0.8],[0.2,0.2,0.9]],
+[[0.6,0.5,0.5],[0.4,0.4,0.4]],
+[[0.1,0.1,0.1],[0.9,0.9,0.9]]]
+
+imshow(Z, interpolation='nearest')
+show()
+
+
+def initialise(nx, ny):
+	''' 
+	nx: number of pixels in the x axis
+	ny: number of pixels in the y axis
+	'''
+	
+
