163cfa2703da80a472828e24fafbc053ce3a62b5
Elizabeth Gallagher <lizgzil@hotmail.com>
2018-07-03 22:46:41 +0100

    working with basic fitness

--- ga.py
+++ ga.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
-import random
+from random import *
 
 from collections import OrderedDict
 
 from pylab import imshow, show, get_cmap
 from numpy import random
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
 
 random.seed(1)
 
@@ -57,17 +61,14 @@ def breed(agent1, agent2, prob_mutation, number_pixels):
 	'''
 
 	random_half_pixels = np.random.choice(range(0, number_pixels), 
-		round((number_pixels)/2), replace = False)
+		round((number_pixels)/2), replace = True)
 
 	baby_agent = {'pixels' : agent1.get('pixels')}
 	for pixel in random_half_pixels:
 		baby_agent['pixels'][pixel]= agent2['pixels'][pixel]
 
-	print('1')
-	print(baby_agent)
-
 	random_pixels = np.random.choice(range(0, number_pixels), 
-		round(prob_mutation*number_pixels), replace = False)
+		round(prob_mutation*number_pixels), replace = True)
 
 	#for pixel in random_pixels:
 	#	baby_agent.get('pixels')[pixel] = random.random(3)
@@ -75,14 +76,10 @@ def breed(agent1, agent2, prob_mutation, number_pixels):
 	baby_agent['pixels'][random_pixels] = random.random(
 		(round(prob_mutation*number_pixels), 3)
 	)
-	print('2')
-	print(baby_agent)
 
 	baby_agent = {'pixels': baby_agent['pixels'], 'fitness': calculate_fitness(
 		baby_agent.get('pixels'), number_pixels
 	)}
-	print('3')
-	print(baby_agent)
 
 	return baby_agent
 
@@ -100,42 +97,30 @@ def make_next_gen(skim_percentage, agents, pop_size, number_pixels):
 
 	fittest_agents = agents_ordered[:number_who_survive]
 
-	new_agents_pixels = []
+	new_agents = []
 	#agents = fittest_agents
-	c = 0
+
 	for rep_agent in range(0,(pop_size - number_who_survive)):
 
-		print('3.5')
-		print(new_agents_pixels)
 		agent1 = random.choice(fittest_agents)
 		agent2 = random.choice(fittest_agents)
-		print('3.6')
-		print(new_agents_pixels)
-		#baby_agent = breed(agent1, agent2, prob_mutation, number_pixels)
-		print('3.7')
-		print(new_agents_pixels)
-		print('4')
+	
 		#print(baby_agent)
 		#new_agents.append(baby_agent)
 		#new_agents['pixels'].append(baby_agent.get('pixels'))
 		#new_agents['fitness'].append(baby_agent.get('fitness'))
 		#new_agents_pixels.append(baby_agent.copy())
-		new_agents_pixels.append(breed(agent1, agent2, prob_mutation, number_pixels))
+		new_agents.append(breed(agent1, agent2, prob_mutation, number_pixels))
 		
 		#agents.append({'pixels':baby_agent.get('pixels'), 'fitness':baby_agent.get('fitness')})
-		print('5')
-		#print(agents)
-		print(new_agents_pixels)
-		c = c+1
 
-	#for new_agents in new_agents_pixels:
+
+	#for new_agents in new_agents:
 	# 	new_agents.update({'fitness' : calculate_fitness(new_agents.get('pixels'), number_pixels)})
 
 
 	#print(new_agents)
-	#agents = fittest_agents + new_agents
-	print('6')
-	print(agents)
+	agents = fittest_agents + new_agents
 	return agents
 
 
@@ -149,56 +134,67 @@ def get_population_mean_fitness(agents):
 
 if __name__ == '__main__':
 
-	nx = 2
-	ny = 2
-	number_pixels = nx*ny
-	pop_size = 4
-	assert pop_size >= 3, "Pick a population size >=3"
-	skim_percentage = 0.3 # percentage of agents who survive each year
-	#percentage_pixels_mutate = 0.1 # What percentage of all pixels get mutated if at all
-	prob_mutation = 0.8
-	num_iterations = 3
+	save_details = []
 
-	# Initialisation:
-	agents = []
-	for agent in range(0, pop_size):
-		agents.append(initialise_agent(number_pixels))
+	for rand_params in tqdm(range(0,100)):
 
-	for agent in agents:
-	 	agent.update({'fitness' : calculate_fitness(agent.get('pixels'), number_pixels)})
+		nx = 10
+		ny = 10
+		number_pixels = nx*ny
+		pop_size = 100
+		assert pop_size >= 3, "Pick a population size >=3"
+		skim_percentage = uniform(0, 1) # percentage of agents who survive each year
+		#percentage_pixels_mutate = 0.1 # What percentage of all pixels get mutated if at all
+		prob_mutation = uniform(0, 1)
+		num_iterations = 1000
 
-	print("population before:")
-	mean_pixel = get_population_mean_fitness(agents)
-	print(mean_pixel)
+		# Initialisation:
+		agents = []
+		for agent in range(0, pop_size):
+			agents.append(initialise_agent(number_pixels))
 
-	print("before")
-	print(agents)
+		for agent in agents:
+		 	agent.update({'fitness' : calculate_fitness(agent.get('pixels'), number_pixels)})
 
-	# Iterations: 
-	for iteration in range(0,num_iterations):
+		#print("population before:")
+		mean_pixel_begin = get_population_mean_fitness(agents)
+		#print(mean_pixel_begin)
 
-		agents = make_next_gen(skim_percentage, agents, pop_size, number_pixels)
+		# Iterations: 
+		for iteration in range(0,num_iterations):
 
-		print('7')
-		print(agents)
+			agents = make_next_gen(skim_percentage, agents, pop_size, number_pixels)
+			#if iteration % 1000 == 0:
+				#print(iteration)
 
-		if iteration % 1000 == 0:
-			print(iteration)
+		#print("population after:")
+		mean_pixel = get_population_mean_fitness(agents)
+		#print(mean_pixel)
+		#print(skim_percentage)
+		#print(prob_mutation)
+		#print(num_iterations)
 
-	print("population after:")
-	mean_pixel = get_population_mean_fitness(agents)
-	print(mean_pixel)
-	print(skim_percentage)
-	print(prob_mutation)
-	print(num_iterations)
+		save_details.append([number_pixels,pop_size,skim_percentage,prob_mutation,num_iterations,mean_pixel_begin,mean_pixel])
 
-	print("after")
-	print(agents)
-	#for ag in agents:
-	 #	print(ag.get('pixels'))
+	save_details = pd.DataFrame(save_details, columns = ['Number of pixels','Population size','skim_percentage','prob_mutation','Number of iterations','Beginning mean pixel','End mean pixel'])
+	#print(save_details)
+	save_details.to_csv('Looking for parameters3.csv')
 
-	 # The pixels are changing, the fitness isn't
+	x = save_details['skim_percentage']
+	y = save_details['prob_mutation']
+	maxs = np.max(save_details['End mean pixel'])
+	mins = np.min(save_details['End mean pixel'])
+	colors = [[(s-mins)/(maxs-mins),0,0] for s in save_details['End mean pixel']]
+	
+	plt.scatter(x, y, c=colors)
+	plt.xlabel("skim_percentage")
+	plt.ylabel("prob_mutation")
+	plt.title("highest is "+str(round(maxs,3))+" (red) lowest is "+str(round(mins,3))+" (black)")
+	plt.show()
 
+	# blue is end result 0 , magenta is 1
+	#plt.scatter(1,1, c=1, alpha=0.5) 
+	#plt.show()
 
 
 
